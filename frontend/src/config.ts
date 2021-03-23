e35e857574b918ef03736f9551ce7ce71166e1c1
Finnfalter <finnfalter@gmail.com>
2021-03-23 21:53:59 +0100

    latest config and template

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 // Once the application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'yu92s114s2'
-//export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
-export const apiEndpoint = `http://localhost:3003/dev`
+const apiId = 'tz4w7uz36c'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
+//export const apiEndpoint = `http://localhost:3003/dev`
 
 export const authConfig = {
   // Create an Auth0 application and copy values from it into this map
